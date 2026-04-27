8e6c8df7014486d256f7646fcd3d99746d986a6e
dmarchak <dustnm@gmail.com>
2026-04-27 12:58:40 -0600

    first commit

---
+++ myapi.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""A simple Flask API that returns a JSON response."""
+
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+
+@app.route('/api', methods=['GET'])
+def get_data():
+    """Endpoint to return a JSON response."""
+    return jsonify({"message": "Hello, World!"})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
